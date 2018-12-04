235f8ae9fd55904111150f42638046430eb5529d
Sam Ho <hos5@rpi.edu>
2018-12-04 16:26:04 -0500

    Tried adding some more steps to the tutorial and running it. It only showed in the console, not in the canvas.

--- site/public/js/controllers/Tour.js
+++ site/public/js/controllers/Tour.js
@@ -3,8 +3,33 @@ var Tour = (function() {
     return {
         ROOT: undefined,
         start: function() {
+            console.log('START TOUR');
             
             var myTour = [
+                {
+                    element: '#items',
+                    popover: {
+                        title: 'File Input Button',
+                        description: 'This side bar is where you can find all the components. Just simply click and drag an element into the rest of the page.',
+                        position: 'right'
+                    }
+                },
+                {
+                    element: '#item-ConstantLow',
+                    popover: {
+                        title: 'Constant Low',
+                        description: 'This provides the circuit with no power',
+                        position: 'right'
+                    }
+                },
+                {
+                    element: '#item-ConstantHigh',
+                    popover: {
+                        title: 'Constant High',
+                        description: 'This provides the circuit with a constant source of power',
+                        position: 'right'
+                    }
+                },
                 {
                     element: '#items',
                     popover: {
@@ -13,17 +38,25 @@ var Tour = (function() {
                         position: 'right'
                     }
                 },
-                {
-                    element: '#file-input',
-                    popover: {
-                        title: 'Step 2',
-                        description: 'this is the description for Step 2',
-                        position: 'right'
-                    }
-                }
+                // {
+                //     element: '#items',
+                //     popover: {
+                //         title: 'Step 1',
+                //         description: 'This is the description for Step 1',
+                //         position: 'right'
+                //     }
+                // },
+                // {
+                //     element: '#items',
+                //     popover: {
+                //         title: 'Step 1',
+                //         description: 'This is the description for Step 1',
+                //         position: 'right'
+                //     }
+                // },
             ]
             
-            var driver = new Driver()
+            var driver = new Driver({ animate: false })
             driver.defineSteps(myTour)
             driver.start()
         }
